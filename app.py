a1ee228af6a2498ccece73b08e798bdf3458be6e
lakshya8052-tech <lakshya8052@gmail.com>
2026-01-30 14:50:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
 st.title("Campus Mess Food Feedback Analyzer")
 
+# Fake dataset (same as before)
 data = {
     "dish": ["Idli", "Rice", "Paneer", "Poha"],
     "rating": [4, 2, 5, 3]
@@ -14,6 +14,7 @@ df = pd.DataFrame(data)
 st.subheader("Mess Food Ratings Data")
 st.dataframe(df)
 
+# Best and worst dish
 best = df.loc[df["rating"].idxmax()]
 worst = df.loc[df["rating"].idxmin()]
 
@@ -24,10 +25,4 @@ st.subheader("Worst Dish")
 st.write(worst)
 
 st.subheader("Ratings Visualization")
-
-fig, ax = plt.subplots()
-ax.bar(df["dish"], df["rating"])
-ax.set_xlabel("Dish")
-ax.set_ylabel("Rating")
-ax.set_title("Mess Food Ratings")
-st.pyplot(fig)
+st.bar_chart(df.set_index("dish"))
